2b15286332dcbf1bb12fe8c498ebfbc5266d1b7b
Kofi Appiah <kofi00appiah@gmail.com>
2025-11-27 19:09:10 +0000

    feat: Create Home page with hero and about sections, integrating styled media galleries.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,45 @@ import React from 'react';
 import MediaGallery from '../components/MediaGallery';
 import '../components/MediaGallery.css';
 import './Home.css';
-import heroImg from '../assets/images/IMG-20251126-WA0000.jpg'; // Using first image as hero/intro image
+import heroImg from '../assets/images/IMG-20251126-WA0000.jpg';
 
-// Vite way to import multiple images
-const imageModules = import.meta.glob('../assets/images/*.{png,jpg,jpeg,webp,svg}', { eager: true });
-const images = Object.keys(imageModules).map((key) => ({
-    src: imageModules[key].default,
-    alt: key.split('/').pop().replace(/\.[^.]+$/, "").replace(/[-_]/g, " ")
-})).slice(0, 10);
+// Top Row Images
+import img49 from '../assets/images/IMG-20251126-WA0049.jpg';
+import img57 from '../assets/images/IMG-20251126-WA0057.jpg';
+import img16 from '../assets/images/IMG-20251126-WA0016.jpg';
+import img12 from '../assets/images/IMG-20251126-WA0012.jpg';
+
+// Bottom Rows Images
+import img00 from '../assets/images/IMG-20251126-WA0000.jpg';
+import img01 from '../assets/images/IMG-20251126-WA0001.jpg';
+import img02 from '../assets/images/IMG-20251126-WA0002.jpg';
+import img03 from '../assets/images/IMG-20251126-WA0003.jpg';
+import img04 from '../assets/images/IMG-20251126-WA0004.jpg';
+import img05 from '../assets/images/IMG-20251126-WA0005.jpg';
+import img06 from '../assets/images/IMG-20251126-WA0006.jpg';
+import img07 from '../assets/images/IMG-20251126-WA0007.jpg';
+import img48 from '../assets/images/IMG-20251126-WA0048.jpg';
+import img36 from '../assets/images/IMG-20251126-WA0036.jpg';
+
+const topImages = [
+    { src: img49, alt: "Gallery Image 49" },
+    { src: img57, alt: "Gallery Image 57" },
+    { src: img16, alt: "Gallery Image 16" },
+    { src: img12, alt: "Gallery Image 12" },
+];
+
+const bottomImages = [
+    { src: img00, alt: "Gallery Image 00" },
+    { src: img01, alt: "Gallery Image 01" },
+    { src: img02, alt: "Gallery Image 02" },
+    { src: img03, alt: "Gallery Image 03" },
+    { src: img04, alt: "Gallery Image 04" },
+    { src: img05, alt: "Gallery Image 05" },
+    { src: img06, alt: "Gallery Image 06" },
+    { src: img07, alt: "Gallery Image 07" },
+    { src: img48, alt: "Gallery Image 48" },
+    { src: img36, alt: "Gallery Image 36" },
+];
 
 const Home = () => {
     return (
@@ -51,7 +82,8 @@ const Home = () => {
 
             <section className="container py-xl">
                 <h2 className="section-title">Our Journey in Pictures</h2>
-                <MediaGallery images={images} />
+                <MediaGallery images={topImages} />
+                <MediaGallery images={bottomImages} />
             </section>
         </div>
     );
